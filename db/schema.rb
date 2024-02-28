a47f6787bdd8d595b0593eb2e284ddf15f697cf3
SekiroXtreme <JosedavidRH30@gmail.com>
2024-02-28 14:05:52 -0500

    add warehouse-products association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_28_000448) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_28_190346) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_28_000448) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "warehouse_id", null: false
+    t.index ["warehouse_id"], name: "index_products_on_warehouse_id"
   end
 
   create_table "providers", force: :cascade do |t|
@@ -97,5 +99,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_28_000448) do
     t.index ["user_id"], name: "index_warehouses_on_user_id"
   end
 
+  add_foreign_key "products", "warehouses"
   add_foreign_key "warehouses", "users"
 end
