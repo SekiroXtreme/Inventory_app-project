ae437efc214a9cff5019cea95a7201b6f5c3f05c
SekiroXtreme <JosedavidRH30@gmail.com>
2024-03-04 16:15:20 -0500

    add changes to products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,18 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[edit show destroy update]
+  before_action :set_product, only: %i[update show destroy ]
+  before_action :set_warehouse, only: %i[update show destroy create]
 
   def index
   end
 
   def new
+    @product = Product.new
   end
 
   def create
-    @product = current_user.products.build(product_params)
+    @product = @warehouse.products.build(product_params)
     if @product.save
-      redirect_to warehouse_product_path(current_user), notice: "Producto añadido con éxito"
+      redirect_to warehouse_products_path(current_user), notice: 'Producto añadido con éxito'
     else
       render :new, status: :unprocessable_entity
     end
@@ -21,30 +23,31 @@ class ProductsController < ApplicationController
 
   def update
     if @product.update(product_params)
-      redirect_to warehouse_product_path(current_user)
+      redirect_to warehouse_products_path(current_user)
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @product.destroy
     if @product.destroy
-      redirect_to warehouse_product_path(current_user)
+      redirect_to warehouse_products_path(current_user)
+    end
   end
 
   private
-    def set_product
-      @product = Product.find(params[:id])
-    end
 
-    def set_warehouse
-      @warehouse = Warehouse.find(params[:id])
-    end
-
-    def product_params
-      params.require(:product).permit(:name, :price, :stock, :description)
-    end
+  def set_product
+    @product = Product.find(params[:id])
   end
 
+  def set_warehouse
+    @warehouse = Warehouse.find(params[:warehouse_id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :stock, :description)
+  end
 end
+
+
